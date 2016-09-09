c959e72b5f43045e05edfaaf26630025b8bb1fe9
Aaron Walker <a.walker@base2services.com>
2016-09-09 13:00:56 +1000

    simple ecr step wrapper

---
+++ vars/withECR.groovy
@@ -0,0 +1,19 @@
+/***********************************
+ECR Step DSL
+
+logs docker service into Elastic Container Registry
+
+example usage
+withECR(1234567890,region) {
+  sh """
+  docker build -t 1234567890.dkr.ecr.${region}.amazonaws.com/myrepo/myapp:${env.BUILD_NUMBER} .
+  docker push 1234567890.dkr.ecr.${region}.amazonaws.com/myrepo/myapp:${env.BUILD_NUMBER}
+  """
+}
+************************************/
+
+def call(awsAccountId, region, body) {
+    sh "eval $(aws ecr get-login --region ${region})"
+    body()
+    sh "docker logout  https://${awsAccountId}.dkr.ecr.${region}.amazonaws.com"
+}
