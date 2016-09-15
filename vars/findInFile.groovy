25463f602e6856c5a44ec1013e2cee42fd978f3b
Aaron Walker <a.walker@base2services.com>
2016-09-15 16:38:30 +1000

    switch to use shell...bit of a hack

--- vars/findInFile.groovy
+++ vars/findInFile.groovy
@@ -18,8 +18,6 @@ def call(pattern, body) {
   def currentDir = config.get('dir',pwd())
   def filePattern = config.get('filenameFilter', '.*')
 
-  sh "printenv && ls -al ${currentDir}"
-  echo "looking for ${pattern} in ${currentDir}/${filePattern}"
   sh "find ${currentDir} -name '${filePattern}' | xargs grep '${pattern}' | cut -d ':' -f 1 | rev | cut -d '/' -f 1 | rev > matches.txt"
   def matching = readFile('matches.txt').split("\r?\n")
   sh 'rm matches.txt'
