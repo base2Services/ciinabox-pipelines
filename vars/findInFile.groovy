9ed01eb9de44eb69fcb6cf197156cff3c661e2f9
Aaron Walker <a.walker@base2services.com>
2016-09-15 13:24:19 +1000

    renames withFile to findInFile

---
+++ vars/findInFile.groovy
@@ -0,0 +1,30 @@
+/***********************************
+with Each File Step DSL
+
+find  a block for each file match a pattern
+
+example usage
+def environments = findInFile("stringineedtofind") {
+  filenameFilter = '*.json'
+}
+************************************/
+
+def call(pattern, body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def currentDir = new File(config.get('dir','.'))
+  def filePattern = config.get('filenameFilter', '.*')
+  def matching = []
+
+  echo "looking for ${pattern} in ${currentDir.absolutePath}/"
+  new FileNameFinder().getFileNames("${currentDir.absolutePath}/",filePattern).each { filename ->
+    def file = new File(filename);
+    if(file.text.contains(pattern)) {
+      matching << filename
+    }
+  }
+  matching
+}
