21786b087418de7cef92f05dc449e6a1f94dd0b9
Nikola Tosic <tosicnikola10@gmail.com>
2016-11-07 10:06:07 +0100

    SlackSend stub

---
+++ vars/slackSend.groovy
@@ -0,0 +1,12 @@
+/***********************************
+ slackSend DSL
+
+ Just a stub method with no operation
+
+ example usage
+ slackSend 'Build $BUILD_NUMBER completed'
+ ************************************/
+
+def call(body) {
+    echo "Slack message stubg method\n\t >>>> $body"
+}
