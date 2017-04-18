e1d0743247cc43b1d769ab021506d202bad63ad1
Nikola Tosic <tosicnikola10@gmail.com>
2017-04-18 11:05:35 +1000

    Add shellOut helper

---
+++ vars/shellOut.groovy
@@ -0,0 +1,15 @@
+/***********************************
+ Shell out DSL
+
+ returns shell out for given command, stripped of trailing
+ line feed
+
+ example usage
+ def awsAccountId=shellOut('aws sts get-caller-identity --query Account --output text)
+ ************************************/
+
+def call(cmd) {
+
+  def out = sh script: cmd, returnStdout: true
+  return out.replace('\n', '')
+}
