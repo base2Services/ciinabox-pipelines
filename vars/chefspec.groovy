813b707911c4f003f1736e9050baba0f2e496fba
Aaron Walker <aaronpwalker@gmail.com>
2017-09-18 11:45:01 +1000

    fixes chefspec var

--- vars/chefspec.groovy
+++ vars/chefspec.groovy
@@ -10,30 +10,29 @@
 def call(body) {
     echo "Slack message stub method\n\t >>>> $body"
     withEnv(["COOKBOOK=${body}"]) {
-
-    }
-    sh '''#!/bin/bash
-    eval "$(/opt/chefdk/bin/chef shell-init bash)"
-    export LC_CTYPE=en_US.UTF-8
-    echo "=========================================="
-    echo "run chef build for cookbook: ${COOKBOOK}"
-    echo "=========================================="
-    cd $WORKSPACE/$COOKBOOK
-    gem install version
-    berks install
-    if [ $? -ne 0 ]; then
-      echo "Berkshelf install Failed!"
-      exit 2
-    fi
-
-    rake test
-    if [ $? -ne 0 ]; then
-      echo "chefspec failed!"
+      sh '''#!/bin/bash
+      eval "$(/opt/chefdk/bin/chef shell-init bash)"
+      export LC_CTYPE=en_US.UTF-8
+      echo "=========================================="
+      echo "run chef build for cookbook: ${COOKBOOK}"
+      echo "=========================================="
+      cd $WORKSPACE/$COOKBOOK
+      gem install version
+      berks install
+      if [ $? -ne 0 ]; then
+        echo "Berkshelf install Failed!"
         exit 2
-    fi
-    echo "=========================================="
-    echo "completed cookbook build ${COOKBOOK}"
-    echo "=========================================="
-    cd $WORKSPACE
-    '''
+      fi
+
+      rake test
+      if [ $? -ne 0 ]; then
+        echo "chefspec failed!"
+          exit 2
+      fi
+      echo "=========================================="
+      echo "completed cookbook build ${COOKBOOK}"
+      echo "=========================================="
+      cd $WORKSPACE
+      '''
+    }
 }
