3aed54fb853b029b1c8ba0369370057d74a8b12d
Aaron Walker <aaronpwalker@gmail.com>
2017-09-18 11:37:43 +1000

    fixes chefspec var

--- vars/chefspec.groovy
+++ vars/chefspec.groovy
@@ -9,28 +9,31 @@
 
 def call(body) {
     echo "Slack message stub method\n\t >>>> $body"
-    sh """#!/bin/bash
-    eval '$(/opt/chefdk/bin/chef shell-init bash)'
+    withEnv(["COOKBOOK=${body}"]) {
+
+    }
+    sh '''#!/bin/bash
+    eval "$(/opt/chefdk/bin/chef shell-init bash)"
     export LC_CTYPE=en_US.UTF-8
-    echo '=========================================='
-    echo 'run chef build for cookbook: ${body}'
-    echo '=========================================='
-    cd ${body}
+    echo "=========================================="
+    echo "run chef build for cookbook: ${COOKBOOK}"
+    echo "=========================================="
+    cd $WORKSPACE/$COOKBOOK
     gem install version
     berks install
     if [ $? -ne 0 ]; then
-      echo 'Berkshelf install Failed!'
+      echo "Berkshelf install Failed!"
       exit 2
     fi
 
     rake test
     if [ $? -ne 0 ]; then
-      echo 'chefspec failed!'
+      echo "chefspec failed!"
         exit 2
     fi
-    echo '=========================================='
-    echo 'completed cookbook build ${body}'
-    echo '=========================================='
-    cd ..
-    """
+    echo "=========================================="
+    echo "completed cookbook build ${COOKBOOK}"
+    echo "=========================================="
+    cd $WORKSPACE
+    '''
 }
