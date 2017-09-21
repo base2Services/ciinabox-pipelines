63befceb1b3abf624232d95a96e61371e1471281
Aaron Walker <aaronpwalker@gmail.com>
2017-09-21 18:00:47 +1000

    adds bake ami functions

--- vars/verifyAMI.groovy
+++ vars/verifyAMI.groovy
@@ -6,17 +6,24 @@
  example usage
  verifyAMI ......
  ************************************/
+ @Grab('org.yaml:snakeyaml:1.18')
+
  def call(body) {
    def config = body
    deleteDir()
    unstash 'baked-ami'
    unstash 'cookbook'
-   withEnv(["ROLE=${config.get('role')}"]) {
+   withEnv(["ROLE=${config.get('role')}", "COOKBOOK=${config.get(cookbook)}"]) {
      sh '''
+       tar xfz cookbooks.tar.gz
        ls -al
        SOURCE_AMI=$(grep 'ami:' ${ROLE}-ami-*.yml | awk -F ':' {'print $2'})
        echo $SOURCE_AMI
+       cd cookbooks/$COOKBOOK
        cat .kitchen.yml
      '''
    }
+}
+
+def createKitchenLocalOverride() {
 }
