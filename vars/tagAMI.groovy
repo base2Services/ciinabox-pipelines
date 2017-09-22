e5a4350fed8da6c76668090c5cb9a3edc2d6c21f
Aaron Walker <aaronpwalker@gmail.com>
2017-09-22 22:47:34 +1000

    adds bake ami functions

---
+++ vars/tagAMI.groovy
@@ -0,0 +1,35 @@
+/***********************************
+ tagAMI DSL
+
+ Tags an AMI
+
+ example usage
+ lookupAMI region: 'ap-southeast-2',  ami: 'xyz', tags: ['status':'verifed']
+ ************************************/
+ @Grab(group='com.amazonaws', module='aws-java-sdk-ec2', version='1.11.198')
+
+ import com.amazonaws.services.ec2.*
+ import com.amazonaws.services.ec2.model.*
+ import com.amazonaws.regions.*
+
+ def call(body) {
+   def config = body
+   addTags(config.region, config.ami, config.tags)
+}
+
+@NonCPS
+def addTags(region, ami, tags) {
+  def ec2 = AmazonEC2ClientBuilder.standard()
+    .withRegion(region)
+    .build()
+
+  def newTags = []
+  tags.each { key, value ->
+    newTags << new Tag(key, value)
+  }
+
+  ec2.createTags(new CreateTagsRequest()
+    .withResources(ami)
+    .withTags(newTags)
+  )
+}
