18de09e8c513a0ba1ba70ee097ce5bac4315f2fc
Samseppiol <j_hage@live.com>
2019-10-01 10:52:30 +1000

    Init for service desired count

---
+++ vars/getDesiredCount.groovy
@@ -0,0 +1,73 @@
+
+
+@Grab(group='com.amazonaws', module='aws-java-sdk-ecs', version='1.11.359')
+@Grab(group='com.amazonaws', module='aws-java-sdk-iam', version='1.11.359')
+@Grab(group='com.amazonaws', module='aws-java-sdk-sts', version='1.11.359')
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicSessionCredentials
+import com.amazonaws.services.ecs.*
+import com.amazonaws.services.ecs.model.DescribeServicesResult
+import groovy.json.JsonSlurper
+import groovy.json.JsonSlurperClassic
+
+import java.util.concurrent.*
+
+def call(body) {
+    def config = body
+    def client = setupECSClient(config.region, config.accountId, config.role)
+    def request = new DescribeServicesRequest()
+    request.withCluster(config.cluster)
+    request.withServices(config.service)
+    def serviceResult = client.DescribeServices(request)
+    println serviceResult
+    return serviceResult
+    // def services = serviceResult.getServices()
+}
+
+
+@NonCPS
+def setupECSClient(region, awsAccountId = null, role = null) {
+  def cb = AmazonECSClientBuilder.standard().withRegion(region)
+  def creds = getCredentials(awsAccountId, region, role)
+  if(creds != null) {
+    cb.withCredentials(new AWSStaticCredentialsProvider(creds))
+  }
+  return cb.build()
+}
+
+@NonCPS
+def getCredentials(awsAccountId, region, roleName) {
+  def env = System.getenv()
+  if(env['AWS_SESSION_TOKEN'] != null) {
+    return new BasicSessionCredentials(
+      env['AWS_ACCESS_KEY_ID'],
+      env['AWS_SECRET_ACCESS_KEY'],
+      env['AWS_SESSION_TOKEN']
+    )
+  } else if(awsAccountId != null && roleName != null) {
+    def stsCreds = assumeRole(awsAccountId, region, roleName)
+    return new BasicSessionCredentials(
+      stsCreds.getAccessKeyId(),
+      stsCreds.getSecretAccessKey(),
+      stsCreds.getSessionToken()
+    )
+  } else {
+    return null
+  }
+}
+
+@NonCPS
+def assumeRole(awsAccountId, region, roleName) {
+  def roleArn = "arn:aws:iam::" + awsAccountId + ":role/" + roleName
+  def roleSessionName = "sts-session-" + awsAccountId
+  println "assuming IAM role ${roleArn}"
+  def sts = new AWSSecurityTokenServiceClient()
+  if (!region.equals("us-east-1")) {
+      sts.setEndpoint("sts." + region + ".amazonaws.com")
+  }
+  def assumeRoleResult = sts.assumeRole(new AssumeRoleRequest()
+            .withRoleArn(roleArn).withDurationSeconds(3600)
+            .withRoleSessionName(roleSessionName))
+  return assumeRoleResult.getCredentials()
+}
