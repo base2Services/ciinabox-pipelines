015fa7463520361fab1a5036240794d8bcb68c22
Lohgan Nash <lohgannash@gmail.com>
2019-11-21 10:07:22 +1100

    ECSTask Refactor wait logic & reauth client (#100)
    * Add debugging
    
    * Refactor wait logic for a running ecs task
    
    * Was using python syntax..
    
    * Amcelcase
    
    * Add optional config for sts session duration
    
    * Added an extra comma
    
    * Set default differently
    
    * Accidently removed the param
    
    * Get new creds if current expires
    
    * Add import
    
    * Logging essage
    
    * Catch creds exception in main try block
    
    * Not actually throwing that exception, its just the error code.
    
    * Move sleep

--- vars/ecsTask.groovy
+++ vars/ecsTask.groovy
@@ -9,7 +9,8 @@ ecsTask (
   role: 'ciinabox',
   launchType: 'FARGATE',
   subnets: ['subnet-12345'],
-  securityGroup: ['sg-12345']
+  securityGroup: ['sg-12345'],
+  credsDuration: '3600'
 )
 ************************************/
 
@@ -28,15 +29,17 @@ import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient
 import com.amazonaws.services.securitytoken.model.AssumeRoleRequest
 import com.amazonaws.waiters.NoOpWaiterHandler
 import com.amazonaws.waiters.WaiterParameters
+import com.amazonaws.services.securitytoken.model.ExpiredTokenException
 
 import java.util.concurrent.*
 
 def call(body) {
+  
   def config = body
-  def client = setupECSClient(config.region, config.accountId, config.role)
-
   config.wait = config.get('wait', false)
+  config.credsDuration = config.get('credsDuration', 3600)
 
+  def client = setupECSClient(config.region, config.accountId, config.role, config.credsDuration)
   handleActionRequest(client, config)
 }
 
@@ -46,6 +49,7 @@ def handleActionRequest(client, config) {
   switch (config.action) {
     case 'runAndWait':
       def startedTasks = startTask(client, config)
+      Thread.sleep(5 * 1000)
       success = wait(client, config, startedTasks)
       break
     default:
@@ -77,30 +81,51 @@ def startTask(client, config) {
   return runResult
 }
 
+
 @NonCPS
-def wait(client, config, startedTasks) {
+def extendedWait(client, config, startedTasks, descRequest) {
+
   def waiter = client.waiters().tasksStopped()
 
-  def describeTasksRequest = new DescribeTasksRequest()
-  describeTasksRequest.withCluster(config.cluster)
-  describeTasksRequest.withTasks(startedTasks.tasks.collectMany { [it.taskArn] })
+  descRequest.withCluster(config.cluster)
+  descRequest.withTasks(startedTasks.tasks.collectMany { [it.taskArn] })
 
-  Thread.sleep(5 * 1000)  // Allow the tasks to start
+  Future future = waiter.runAsync(
+    new WaiterParameters<>(descRequest),
+    new NoOpWaiterHandler()
+  )
+  while(!future.isDone()) {
+    try {
+      println "waiting for task to complete"
+      Thread.sleep(5 * 1000)
+    } catch(InterruptedException ex) {
+      println "We seem to be timing out ${ex}...ignoring"
+    }
+  }
+
+  def taskCurrentState = client.describeTasks(descRequest)
+  for(task in taskCurrentState.tasks) {
+    if(task.lastStatus == "RUNNING") {
+      return false
+    } else { return true }
+  }
+
+}
+
+@NonCPS
+def wait(client, config, startedTasks) {
+
+  def describeTasksRequest = new DescribeTasksRequest()
+  def taskComplete = false
+
   try {
-    Future future = waiter.runAsync(
-      new WaiterParameters<>(describeTasksRequest),
-      new NoOpWaiterHandler()
-    )
-    while(!future.isDone()) {
-      try {
-        println "waiting for task to complete"
-        Thread.sleep(5 * 1000)
-      } catch(InterruptedException ex) {
-        println "We seem to be timing out ${ex}...ignoring"
-      }
+
+    while(!taskComplete) {
+      taskComplete = extendedWait(client, config, startedTasks, describeTasksRequest)
     }
 
     def taskDescriptions = client.describeTasks(describeTasksRequest)
+    println taskDescriptions
     if (taskDescriptions.tasks.size() != 1) {
       println "Couldn't find launched task"
       return false
@@ -115,15 +140,22 @@ def wait(client, config, startedTasks) {
     }
     return true
   } catch(Exception e) {
-      println "Waiting for task failed. - ${e}"
-      return false
+      if(e.getErrorCode() == "ExpiredTokenException") {
+        println "Credentials have expired, reinitialising client..."
+        client = setupECSClient(config.region, config.accountId, config.role, config.credsDuration)
+        wait(client, config, startedTasks) 
+      }
+      else {
+        println "Waiting for task failed. - ${e}"
+        return false
+      }
   }
 }
 
 @NonCPS
-def setupECSClient(region, awsAccountId = null, role = null) {
+def setupECSClient(region, awsAccountId = null, role = null, credsDuration) {
   def cb = AmazonECSClientBuilder.standard().withRegion(region)
-  def creds = getCredentials(awsAccountId, region, role)
+  def creds = getCredentials(awsAccountId, region, role, credsDuration)
   if(creds != null) {
     cb.withCredentials(new AWSStaticCredentialsProvider(creds))
   }
@@ -131,7 +163,7 @@ def setupECSClient(region, awsAccountId = null, role = null) {
 }
 
 @NonCPS
-def getCredentials(awsAccountId, region, roleName) {
+def getCredentials(awsAccountId, region, roleName, credsDuration) {
   def env = System.getenv()
   if(env['AWS_SESSION_TOKEN'] != null) {
     return new BasicSessionCredentials(
@@ -140,7 +172,7 @@ def getCredentials(awsAccountId, region, roleName) {
       env['AWS_SESSION_TOKEN']
     )
   } else if(awsAccountId != null && roleName != null) {
-    def stsCreds = assumeRole(awsAccountId, region, roleName)
+    def stsCreds = assumeRole(awsAccountId, region, roleName, credsDuration)
     return new BasicSessionCredentials(
       stsCreds.getAccessKeyId(),
       stsCreds.getSecretAccessKey(),
@@ -152,16 +184,16 @@ def getCredentials(awsAccountId, region, roleName) {
 }
 
 @NonCPS
-def assumeRole(awsAccountId, region, roleName) {
+def assumeRole(awsAccountId, region, roleName, credsDuration) {
   def roleArn = "arn:aws:iam::" + awsAccountId + ":role/" + roleName
   def roleSessionName = "sts-session-" + awsAccountId
-  println "assuming IAM role ${roleArn}"
+  println "assuming IAM role ${roleArn}, expiring in ${credsDuration} seconds."
   def sts = new AWSSecurityTokenServiceClient()
   if (!region.equals("us-east-1")) {
       sts.setEndpoint("sts." + region + ".amazonaws.com")
   }
   def assumeRoleResult = sts.assumeRole(new AssumeRoleRequest()
-            .withRoleArn(roleArn).withDurationSeconds(3600)
+            .withRoleArn(roleArn).withDurationSeconds(credsDuration)
             .withRoleSessionName(roleSessionName))
   return assumeRoleResult.getCredentials()
 }
