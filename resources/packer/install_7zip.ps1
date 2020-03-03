1632528513d61cf00711f1a94403b245e07b9f94
Guslington <guslington@gmail.com>
2020-03-03 17:44:58 +1100

    packer template builder with scripts

---
+++ resources/packer/install_7zip.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+  Silently install 7-Zip
+.DESCRIPTION
+  Install 7-Zip using powershell during a packer bake
+  7-Zip source - http://www.7-zip.org/download.html
+.PARAMETER version
+  7-Zip version, defaults to 1900 -> https://www.7-zip.org/a/7z1900-x64.msi
+#>
+
+param (
+  [string]$version = "1900"
+)
+
+$ErrorActionPreference = "Stop"
+Set-ExecutionPolicy Bypass -force
+
+$workdir = "c:\temp\"
+
+# Check if work directory exists if not create it
+if (Test-Path -Path $workdir -PathType Container) {
+  Write-Host "$workdir already exists"
+} else {
+  New-Item -Path $workdir  -ItemType directory
+}
+
+# Download the installer
+$source = "http://www.7-zip.org/a/7z$version-x64.msi"
+$destination = "$workdir\7-Zip.msi"
+Write-Host "INFO: Downloading 7-Zip $version msi from $source"
+Invoke-WebRequest $source -OutFile $destination
+
+Write-Host "INFO: Installing 7-Zip msi"
+$MSIArguments = @(
+  "/i"
+  "$workdir\7-Zip.msi"
+  "/qb"
+)
+Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
+
+Write-Host "INFO: Install complete. Cleaning up"
+rm -Force $workdir\7*
+
+# Add 7zip to path
+Write-Output "INFO: Adding command 7z to the path"
+[Environment]::SetEnvironmentVariable("PATH","$env:path;$env:programfiles\7-Zip","MACHINE")
+$env:path = "$env:path;$env:programfiles\7-Zip"
+
+Write-Output "INFO: 7zip msi install complete"
---
+++ src/com/base2/ciinabox/PackerTemplateBuilder.groovy
@@ -0,0 +1,172 @@
+package com.base2.ciinabox
+
+import groovy.json.JsonOutput
+
+class PackerTemplateBuilder implements Serializable {
+  
+  String id
+  String date
+  String type
+  Map builder = [:]
+  ArrayList provisioners = []
+  
+  PackerTemplateBuilder(String name, String type = 'linux') {
+    this.id = 'bake-' + UUID.randomUUID().toString()
+    this.type = type
+    def now = new Date()
+    this.date = now.format("yyyy-MM-dd't'HH-mm-ss.SSS'z'", TimeZone.getTimeZone('UTC'))
+    def amiName = "${name}-${this.date}"
+    
+    this.builder.type = 'amazon-ebs'
+    this.builder.ami_name = amiName
+    this.builder.run_tags = [
+      "Status": "Baking",
+      "Name": "Packer ${amiName}",
+      "BakeId": this.id,
+      "Role": name
+    ]
+    this.builder.tags = [
+      "Status": "Baked",
+      "Name": amiName,
+      "BakeId": this.id,
+      "Role": name
+    ]
+  }
+  
+  public void addCommunicator(String username = 'ec2-user') {
+    if (this.type.startsWith('windows')) {
+      this.builder.communicator = 'winrm'
+      this.builder.winrm_username = 'Administrator'
+      this.builder.windows_password_timeout = '20m'
+      this.builder.user_data_file = 'setup_winrm.ps1'
+    } else {
+      this.builder.communicator = 'ssh'
+      this.builder.ssh_username = username
+      this.builders.ssh_pty = true
+      this.builders.ssh_timeout = '5m'
+    }
+  }
+
+  public void addBlockDevice(String deviceName, String volumeSize) {
+    def block_device_mapping = [
+      device_name: deviceName,
+      volume_size: ebsVolumeSize,
+      delete_on_termination: true,
+      volume_type: "gp2"
+    ]
+    this.builder.ami_block_device_mappings = [block_device_mapping]
+    this.builder.launch_block_device_mappings = [block_device_mapping]
+  }
+  
+  public void addChefSoloProvisioner(List runList, String json, String version) {
+    def chefProvisioner = [
+      type: 'chef-solo',
+      chef_license: 'accept-silent',
+      run_list: runList
+    ]
+    
+    if (json) {
+      chefProvisioner.json = json
+    }
+    
+    if (version) {
+      chefProvisioner.version = version
+    }
+    
+    if (this.type.startsWith('windows')) {
+      chefProvisioner.remote_cookbook_paths = ["C:/chef/cookbooks"]
+      chefProvisioner.guest_os_type = 'windows'
+      this.provisioners.push([
+        type: 'powershell',
+        inline: [
+          "New-Item c:/chef/cookbooks -type directory -force",
+          "New-Item c:/chef/environments -type directory -force"
+        ]
+      ])
+    } else {
+      chefProvisioner.remote_cookbook_paths = ["/etc/chef/cookbooks"]
+      this.provisioners.push([
+        type: 'shell',
+        inline: [
+          "sudo mkdir -p /etc/chef",
+          "sudo chmod -R 777 /etc/chef"
+        ]
+      ])
+      this.provisioners.push([
+        type: 'file',
+        source: '{{pwd}}/cookbooks',
+        destination: '/etc/chef'
+      ])
+      this.provisioners.push([
+        type: 'file',
+        source: '{{pwd}}/environments',
+        destination: '/etc/chef'
+      ])
+    }
+    
+    this.provisioners.push(chefProvisioner)
+  }
+  
+  public void addDownloadCookbookProvisioner(String bucket, String region, String path, String script = 'download_cookbooks.ps1') {
+    if (this.type.startsWith('windows')) {
+      Map provisioner = [
+        type: 'powershell',
+        script: script,
+        environment_vars: [
+          "SOURCE_BUCKET=${bucket}",
+          "BUCKET_REGION=${region}",
+          "COOKBOOK_PATH=${path}"
+        ]
+      ]
+      this.provisioners.push(provisioner)
+    }
+  }
+  
+  public void addInstall7zipProvisioner(String script = 'install_7zip.ps1') {
+    if (this.type.startsWith('windows')) {
+      Map provisioner = [
+        type: 'powershell',
+        script: script
+      ]
+      this.provisioners.push(provisioner)
+    }
+  }
+  
+  public void addAmamzonConfigProvisioner() {
+    if (this.type.equals('windows')) {
+      this.provisioners.push([
+        type: 'powershell',
+        script: 'ec2_launch_config.ps1'
+      ])
+      this.provisioners.push([
+        type: 'powershell',
+        inline: ['C:/ProgramData/Amazon/EC2-Windows/Launch/Scripts/InitializeInstance.ps1 -Schedule']
+      ])
+    } else if (this.type.equals('windows2012')) {
+      this.provisioners.push([
+        type: 'powershell',
+        script: 'ec2_config_service.ps1'
+      ])
+    }
+    
+  }
+  
+  public String toJson() {
+    Map template = [
+      builders: [],
+      provisioners: [],
+      "post-processors": [
+        [
+          type: 'manifest',
+          output: 'manifest.json',
+          strip_path: true
+        ]
+      ]
+    ]
+    
+    template.builders.push(this.builder)
+    template.provisioners = this.provisioners
+    
+    return JsonOutput.prettyPrint(JsonOutput.toJson(template))
+  }
+}
