734d750a616843f3d1c0386faebf0e3e9add04de
Aaron Walker <aaronpwalker@gmail.com>
2020-03-23 15:40:22 +0100

    adds step for running a AWS step function state machine

--- src/com/base2/ciinabox/AwsClient.groovy
+++ src/com/base2/ciinabox/AwsClient.groovy
@@ -3,6 +3,12 @@ package com.base2.ciinabox
 import com.cloudbees.groovy.cps.NonCPS
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder
+import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicSessionCredentials
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest
 
 class AwsClient {
 
@@ -18,6 +24,66 @@ class AwsClient {
     return AWSSimpleSystemsManagementClientBuilder.standard()
       .withRegion(region)
       .build()
+  }
+
+  @NonCPS
+  static def stepfunctions(config) {
+    def creds = getCredentials(config.accountId, config.region, config.role, config)
+    def cb = AWSStepFunctionsClientBuilder.standard()
+      .withRegion(config.region)
+    if(creds != null) {
+      cb.withCredentials(new AWSStaticCredentialsProvider(creds))
+    }
+    return cb.build()
+  }
+
+  @NonCPS
+  static def getCredentials(awsAccountId, region, roleName, options) {
+    def env = System.getenv()
+    if(env['AWS_SESSION_TOKEN'] != null) {
+      return new BasicSessionCredentials(
+        env['AWS_ACCESS_KEY_ID'],
+        env['AWS_SECRET_ACCESS_KEY'],
+        env['AWS_SESSION_TOKEN']
+      )
+    } else if(awsAccountId != null && roleName != null) {
+      def stsCreds = assumeRole(awsAccountId, region, roleName, options)
+      return new BasicSessionCredentials(
+        stsCreds.getAccessKeyId(),
+        stsCreds.getSecretAccessKey(),
+        stsCreds.getSessionToken()
+      )
+    } else {
+      return null
+    }
+  }
+
+  @NonCPS
+  static def assumeRole(awsAccountId, region, roleName, options) {
+    def roleArn = "arn:aws:iam::" + awsAccountId + ":role/" + roleName
+    def roleSessionName = "sts-session-" + awsAccountId
+    println "assuming IAM role ${roleArn}"
+    def sts = new AWSSecurityTokenServiceClient()
+    if (!region.equals("us-east-1")) {
+        sts.setEndpoint("sts." + region + ".amazonaws.com")
+    }
+
+    def credsDuration = options.get('credsDuration', 3600)
+    def request = new AssumeRoleRequest()
+          .withRoleArn(roleArn).withDurationSeconds(credsDuration)
+          .withRoleSessionName(roleSessionName)
+
+    if(options.externalId) {
+      request.withExternalId(options.externalId)
+    }
+
+    if(options.mfaToken && options.serialNumber) {
+      request.withTokenCode(options.mfaToken)
+      request.withSerialNumber(options.serialNumber)
+    }
+
+    def assumeRoleResult = sts.assumeRole(request)
+    return assumeRoleResult.getCredentials()
   }
 
 }
---
+++ vars/stepFunction.groovy
@@ -0,0 +1,82 @@
+/************************************
+stepFunction (
+  action: 'start',
+  stateMachine: 'my-state-machine',
+  region: 'us-east-1',
+  accountId: '12345678',
+  role: 'ciinabox',
+  parameters:[
+    myparam: 'myvalue',
+    otherparam: 'othervalue'
+  ]
+)
+************************************/
+import com.base2.ciinabox.AwsClient
+import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder
+import com.amazonaws.services.stepfunctions.model.StartExecutionRequest
+import com.amazonaws.services.stepfunctions.model.DescribeExecutionRequest
+import groovy.json.JsonOutput
+
+def call(config) {
+  def client = AwsClient.stepfunctions(config)
+  switch(config.action) {
+    case 'start':
+      def executionArn = handleStart(client, config)
+      println "Started ${executionArn} for ${config.stateMachine}"
+      return executionArn
+      break
+    case 'wait':
+      def status = handleWait(client, config)
+      return status
+      break
+    case 'startAndWait':
+      def executionArn = handleStart(client, config)
+      println "Started ${executionArn} for ${config.stateMachine}"
+      config['executionArn'] = executionArn
+      def status = handleWait(client, config)
+      return status
+      break
+  default:
+    throw new GroovyRuntimeException("The specified action '${config.action}' is not implemented.")
+  }
+
+}
+
+@NonCPS
+def handleStart(client, config) {
+  def params = [input: [:]]
+  if(config.parameters) {
+    params['input'] = config.parameters
+  }
+  def input = JsonOutput.toJson(params)
+  println "Starting State Machine ${config.stateMachine} with input ${input}"
+  def result = client.startExecution(new StartExecutionRequest()
+    .withStateMachineArn("arn:aws:states:${config.region}:${config.accountId}:stateMachine:${config.stateMachine}")
+    .withInput(input)
+  )
+  return result.getExecutionArn()
+}
+
+@NonCPS
+def handleWait(client, config) {
+  def finished = false
+  def status = null
+  while(!finished) {
+    def result = client.describeExecution(new DescribeExecutionRequest()
+      .withExecutionArn(config.executionArn)
+    )
+    status = result.getStatus()
+    println "Waiting for ${config.executionArn} current status ${status}"
+    finished = !result.getStatus().equals('RUNNING')
+    if(!finished) {
+      try {
+        Thread.sleep(30000)
+      } catch (InterruptedException ex){} //ignore
+    }
+  }
+  println "Task exited with status ${status}"
+  if(status != 'SUCCEEDED') {
+    error("Step function ${config.stateMachine} failed, see cloudwatch logs for details")
+  }
+  return status
+}
