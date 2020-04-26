7361aee04b18993c44844f1b6469220899b5b300
Guslington <guslington@gmail.com>
2020-04-27 09:34:53 +1000

    rebuild to use the sdk

--- src/com/base2/ciinabox/AwsHelper.groovy
+++ src/com/base2/ciinabox/AwsHelper.groovy
@@ -1,27 +1,27 @@
 package com.base2.ciinabox
 
-
-import com.amazonaws.auth.*
-import com.amazonaws.regions.*
-import com.amazonaws.waiters.*
-import com.amazonaws.services.securitytoken.*
-import com.amazonaws.services.securitytoken.model.*
+import com.amazonaws.regions.Region
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
+import com.amazonaws.services.securitytoken.model.GetCallerIdentityRequest
 
 /**
- * Return AWS Region from Instance Metadata
+ * Return AWS Region for the current region if on EC2 other wise will return sydney
  */
-def awsRegion() {
-  return shellOut('curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r \'.region\'')
+def getRegion() {
+  def region = Regions.getCurrentRegion()
+  if (region == null) {
+    region = Region.getRegion(Regions.AP_SOUTHEAST_2)
+  }
+  return region
 }
 
 /**
  * Return AWS Account ID gathered from default credentials
  * @return
  */
-def accountId() {
-  def region = awsRegion()
-  shellOut("aws sts get-caller-identity --region ${region} --query Account --output text")
-}
-
-
-
+ def getAccountId() {
+   def sts = AWSSecurityTokenServiceClientBuilder.standard()
+     .withRegion(getRegion())
+     .build()
+   return sts.getCallerIdentity(new GetCallerIdentityRequest()).account
+ }
