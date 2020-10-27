c5084fe7da63e2cfad1dc00fb6667f3e3308432d
Samseppiol <j_hage@live.com>
2020-10-27 17:24:11 +1100

    Assume cross account role within function

--- vars/createEKSIAMAuth.groovy
+++ vars/createEKSIAMAuth.groovy
@@ -9,6 +9,8 @@ agent {
 createEKSIAMAuth(
   region: 'ap-southeast-2',
   cluster: 'my-cluster-name',
+  accountId: 123456789,
+  role: 'roleToAssume',
   identities: [
     [
         arn: "my_role_arn",
@@ -34,27 +36,34 @@ def call(body) {
   if (!config.identities) {
     error('Identities must be defined')
   }
+  if (!config.accountId) {
+    error('accountId must be defined')
+  }
+  if (!config.role) {
+    error('role must be defined')
+  }
 
+  withAWS(role: config.role, roleAccount: config.accountId, roleSessionName: 'eks-apply-auth', region: config.region) {
+    sh "aws eks --region ${config.region} update-kubeconfig --name ${config.cluster}"
+    config.identities.each { identity ->
+      authStatus = sh(
+        returnStatus: true,
+        script: "eksctl get iamidentitymapping --cluster ${config.cluster} --region ${config.region} --arn ${identity.arn}"
+      )
+      println("Got auth status as ${authStatus}")
+      if (authStatus != 1) {
+        println("${identity.arn} already exists within aws-auth config map, skipping...")
+        return
+      }
 
-  config.identities.each { identity ->
-    authStatus = sh(
-      returnStatus: true,
-      script: "eksctl get iamidentitymapping --cluster ${config.cluster} --region ${config.region} --arn ${identity.arn}"
-    )
-    println("Got auth status as ${authStatus}")
-    if (authStatus != 1) {
-      println("${identity.arn} already exists within aws-auth config map, skipping...")
-      return
+      def createCommand = "eksctl create iamidentitymapping  ${config.cluster} --region ${config.region} --arn ${identity.arn}"
+      def groups = ""
+      identity.groups.each { group ->
+        groups += "--group ${group} "
+      }
+      println("Running command ${createCommand} ${groups}")
+      sh "${createCommand} ${groups}"
     }
-
-
-    def createCommand = "eksctl create iamidentitymapping  ${config.cluster} --region ${config.region} --arn ${identity.arn}"
-    def groups = ""
-    identity.groups.each { group ->
-      groups += "--group ${group} "
-    }
-    println("Running command ${createCommand} ${groups}")
-    sh "${createCommand} ${groups}"
   }
 }
 
