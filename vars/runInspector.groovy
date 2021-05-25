4dd2cacb5096947c8f93620fd437047d35e4d489
oblivion-21 <djpa122@pm.me>
2021-05-25 14:15:43 +1000

    fixed some syntax issues

--- vars/runInspector.groovy
+++ vars/runInspector.groovy
@@ -82,7 +82,7 @@ def getResults(String result_arn) {
       def client = AmazonInspectorClientBuilder.standard().build()
       // String format = HTML
       // def fileFormat = ReportFileFormat.fromValue(format)
-      def request = new GetAssessmentReportRequest().withAssessmentRunArn(result_arn).setReportFileFormat(HTML)
+      def request = new GetAssessmentReportRequest().withAssessmentRunArn(result_arn).setReportFileFormat(reportFileFormat.valueOf(HTML))
       def response = client.getAssessmentReport(request)
       println(response)
       return response
