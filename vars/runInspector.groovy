b534871a982e84276eaf5accf3c90d12df44d3d9
oblivion-21 <djpa122@pm.me>
2021-05-28 13:51:49 +1000

    fixed some syntax issues

--- vars/runInspector.groovy
+++ vars/runInspector.groovy
@@ -61,7 +61,7 @@ def call(body) {
 
 
 def installInspectorAgent(String id) {
-      println("instance id in function: ${id}")
+      println("instance id in function: *${id}*")
       def client = AWSSimpleSystemsManagementClientBuilder.standard().build()
       def request = new SendCommandRequest()
             .withInstanceIds(id)
