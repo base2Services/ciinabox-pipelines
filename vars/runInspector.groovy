e591f5eb5bea0f66d0eba4dba8bb48abd35066fb
oblivion-21 <djpa122@pm.me>
2021-06-01 11:23:13 +1000

    fixed some syntax issues

--- vars/runInspector.groovy
+++ vars/runInspector.groovy
@@ -18,7 +18,7 @@ def call(body) {
 
       // Lunch AMI into cloudformaiton stack with sarrounding infrustructure to support scans
 
-      File template = libraryResource('Inspector.yaml')
+      File template = libraryResource('Inspector.yaml').toFile()
       def fileName = 'Inspector.yaml'
       writeFile(file: fileName, text: template)
       def stackName = 'InspectorAmiTest'
