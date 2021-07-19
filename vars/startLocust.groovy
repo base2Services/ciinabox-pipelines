6ef4c8c1867c5691fafaf241e6136240ff46f3b7
Samseppiol <j_hage@live.com>
2021-07-19 13:40:33 +1000

    Init for locust step

---
+++ vars/startLocust.groovy
@@ -0,0 +1,88 @@
+/***********************************
+startLocust DSL
+
+starts locust on fargate
+
+example usage
+
+startLocust(
+  locustMode: master // (optional, defaults to standalone)
+  taskRole: 'arn:aws:iam:accountid::role/taskRole' // required, potentially create if not specified?
+  locustS3Path: mybucketname/pathtofile // (Required) S3 bucket name and path to file
+  locustFile: fileNameInS3 // (Required) locust tests file name
+  hostUrl: https://myhost.example.com // (Required) The applications host name to test
+  version: '0.12.5', // (optional, defaults to 0.10.0)
+  region: 'us-east-1', // (optional, defaults to look up the current region)
+  subnet: 'subnet-123456789', // (optional, defaults to look up the current subnet)
+  securityGroup: 'sg-123456789', // (optional, defaults to look up the current security group)
+  executionRole: 'arn:aws:iam:accountid::role/executionRole', // (optional, defaults to look up the current executionRole)
+  cluster: 'my-cluster', // (optional, defaults to look up the current ecs cluster)
+  cpu: '512', // (optional, defaults to 512)
+  memory: '1024', // (optional, defaults to 1024)
+)
+
+
+************************************/
+
+import com.base2.ciinabox.aws.AwsClientBuilder
+import com.base2.ciinabox.InstanceMetadata
+import com.base2.ciinabox.GetInstanceDetails
+import com.base2.ciinabox.GetEcsContainerDetails
+import com.base2.ciinabox.EcsTaskRunner
+
+def call(body=[:]) {
+  def config = body
+  def details = [:]
+
+  def metadata = new InstanceMetadata()
+  // if the node is a ec2 instance using the ec2 plugin
+  def instanceId = env.NODE_NAME.find(/i-[a-zA-Z0-9]*/)
+
+  if (!instanceId) {
+    instanceId = metadata.instanceId()
+  }
+
+  def region = config.get('region', metadata.region())
+
+  def instance = new GetInstanceDetails(region, instanceId)
+  details.subnet = config.get('subnet', instance.subnet())
+  details.securityGroup = config.get('securityGroup', instance.securityGroup())
+
+  def task = new GetEcsContainerDetails(region)
+  details.executionRole = config.get('executionRole', task.executionRole())
+  details.cluster = config.get('cluster', task.cluster())
+
+  details.cpu = config.get('cpu', '512')
+  details.memory = config.get('memory', '1024')
+  def version = config.get('version', '0.10.0')
+
+  def locustMode = config.get('locustMode', 'standalone')
+
+  def taskDef = []
+  taskDef << [
+    name: 'locust-master',
+    image: "base2/locust:${version}",
+    ports: [
+      [container: 8089, host: 8089],
+      [container: 5557, host: 5557],
+      [container: 5558, host: 5558]
+    ],
+    environment: [
+      [key: 'AWS_REGION', value: region],
+      [key: 'LOCUST_S3_PATH', value: config.locustS3Path],
+      [key: 'LOCUST_FILE', value: config.locustFile],
+      [key: 'LOCUST_MODE', value: locustMode],
+      [key: 'HOST_URL', value: config.hostUrl]
+    ]
+  ]
+
+  def client = new AwsClientBuilder([region: region]).ecs()
+  def runner = new EcsTaskRunner('locust', client)
+  def taskArn = runner.startTask(details, taskDef)
+  def endpoint = runner.getEndpoint(taskArn, details.cluster)
+
+  env['LOCUST_ENDPOINT'] = endpoint
+  env['LOCUST_PORT'] = '8089'
+  env['LOCUST_SOCKET'] = "${endpoint}:8089"
+  env['LOCUST_TASK'] = taskArn
+}
