087ed97971cb96a0bbe967763f0c84775e8ac593
Guslington <guslington@gmail.com>
2021-07-21 14:44:56 +1000

    improve region lookup

--- src/com/base2/ciinabox/aws/Util.groovy
+++ src/com/base2/ciinabox/aws/Util.groovy
@@ -1,7 +1,9 @@
 package com.base2.ciinabox.aws
 
-import com.amazonaws.regions.Regions
-import com.amazonaws.regions.Region
+import com.amazonaws.regions.AwsRegionProviderChain
+import com.amazonaws.regions.AwsEnvVarOverrideRegionProvider
+import com.amazonaws.regions.InstanceMetadataRegionProvider
+import com.amazonaws.SdkClientException
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
 import com.amazonaws.services.securitytoken.model.GetCallerIdentityRequest
 
@@ -9,18 +11,22 @@ class Util implements Serializable {
   
   /**
    * Return AWS Region for the current region if on EC2 other wise will return sydney
+   * @return aws region string
    */
   static def getRegion() {
-    def region = Regions.getCurrentRegion()
-    if (region == null) {
-      region = Region.getRegion(Regions.AP_SOUTHEAST_2)
+    def region = null
+    try {
+      def chain = new AwsRegionProviderChain(new InstanceMetadataRegionProvider(), new AwsEnvVarOverrideRegionProvider())
+      region = chain.getRegion()
+    } catch (SdkClientException ex) {
+      println ex.getMessage()
     }
-    return region.getName()
+    return region
   }
 
   /**
    * Return AWS Account ID gathered from default credentials
-   * @return
+   * @return aws account id string
    */
    static def getAccountId() {
      def sts = AWSSecurityTokenServiceClientBuilder.standard()
