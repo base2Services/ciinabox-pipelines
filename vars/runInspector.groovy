81dc73d37fbfdfa32839b14498e50202eb357cde
oblivion-21 <d.goncalves@base2services.com>
2021-08-03 15:27:24 +1000

    change ciinabox library

--- vars/runInspector.groovy
+++ vars/runInspector.groovy
@@ -38,6 +38,7 @@ def call(body) {
     try{
         findings = main(body, stackName, bucketName, fileName)
     } catch(Exception e) {
+        println(e)
         println("inspector failed to complete it's run, cleaning up resources before erroring out")
 <<<<<<< HEAD
         cleanUp(stackName, body.region, bucketName, fileName)
