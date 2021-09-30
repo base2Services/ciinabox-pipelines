37862587ecd7e46ddfbdeb69c2c9805c29dd5478
Kyle Thorne <k.thorne@base2services.com>
2021-09-30 16:38:58 +1000

    Removed debug outputs

--- vars/cloudformation.groovy
+++ vars/cloudformation.groovy
@@ -199,14 +199,11 @@ def queryStackOutput(cf, config){
 def queryStackExport(cf, config){
   def result = cf.listExports(new ListExportsRequest())
   def export = result.getExports().find { it.getName().equals(config.query.toString()) }
+  
   while(result.nextToken != null && export == null) {
     result = cf.listExports(new ListExportsRequest().withNextToken(result.nextToken));
     export = result.getExports().find { it.getName().equals(config.query.toString()) }
-    println "Result: ${result}."
   }
-  println "Result: ${result}."
-  println "Query: ${config.query.toString()}"
-
 
   if (export == null){
     throw new GroovyRuntimeException("Unable to find cloudformation export ${config.query}")
