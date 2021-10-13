14377f699881d716780341b02a6569f1c57b2898
Samseppiol <j_hage@live.com>
2021-10-13 11:44:21 +1100

    Remove env vars in jenkinsfile

--- vars/jenkMon.groovy
+++ vars/jenkMon.groovy
@@ -1,19 +1,8 @@
 pipeline {
-    parameters {
-        string(name: 'JENKINS_HOST',  description: 'Full jenkins host name for cloudwatch identifier')
-        string(name: 'AWS_DEFAULT_REGION',  description: 'Default region for SDK to run')
-    }
-    environment {
-        JENKINS_HOST = "${params.JENKINS_HOST}"
-        AWS_DEFAULT_REGION = "${params.AWS_DEFAULT_REGION}"
-    }
-
     agent {
         label 'docker'
     }
-
     stages {
-
         stage('Publish Success Metric to CloudWatch') {
             agent {
                 docker {
@@ -24,6 +13,5 @@ pipeline {
                 sh "python /app/jenkmon.py"
             }
         }
-
     }
 }
