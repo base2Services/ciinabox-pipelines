82ceddafbecc55e1f16fbd8e828716eb9a64670d
Mitch Clarke <39038062+mitchclarkebase2@users.noreply.github.com>
2025-12-22 14:58:42 +1100

    Fix syntax of dBSnapshotAvailable

--- vars/takeSnapshot.groovy
+++ vars/takeSnapshot.groovy
@@ -107,7 +107,7 @@ def handleDBCluster(client, config) {
 
 def wait(clientBuilder, snapshotIdentifier, config) {
   def rdsclient = clientBuilder.rds()
-  def waiter = rdsclient.waiters().dbSnapshotAvailable()
+  def waiter = rdsclient.waiters().dBSnapshotAvailable()
   def count = 0
 
   try {
